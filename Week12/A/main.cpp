ebd294f42970b78ced3d338e15f9e6ea2c42cd89
Eduard Pop <eduard.pop@tum.de>
2017-01-25 19:34:22 +0100

    Fixed problem A, week 11

--- Week12/A/main.cpp
+++ Week12/A/main.cpp
@@ -4,55 +4,51 @@
 #include <string>
 using namespace std;
 
-#define iterateEdges(node) for(map<char, Node>::iterator it = node.edges.begin(); it!=node.edges.end(); it++)
+int ct;
 struct Node{
-  bool word;
-  map<char, Node> edges;
-  int words;
+	bool word, conflict;
+	map<char, Node> edges;
+	int words; 
 
-  int addWord(string s)
-  {
-    if(s.length() == 0)
-    {
-      this->word = true;
-      return 0;
-    }
-    if(this->word)
-      words++;
-    if(edges.find(s[0]) == edges.end())
-      edges[s[0]] = Node();
-    words += edges[s[0]].addWord(s.substr(1, s.length()-1));
-    return words;
-  }
-  Node(): word(false), words(0){};
+	void addWord(string s)
+	{
+		words++;
+		if(s.length() == 0)
+		{
+			word = true;
+			if(words > 1)
+			{
+				conflict = true;
+				ct++;
+			}
+			return;
+		}
+		if(this->word && !conflict)
+		{
+			ct++;
+			conflict = true;
+		}
+		if(edges.find(s[0]) == edges.end())
+			edges[s[0]] = Node();
+		edges[s[0]].addWord(s.substr(1));
+	}
+	Node(): word(false), words(0), conflict(false){};
 };
-int count(Node x)
-{
-  int ct = 0;
-  if(x.word)
-    ct+=(x.words > 1)?1:0;
-  iterateEdges(x)
-    ct+=count(it->second);
-  return ct;
-}
 int solve()
 {
-  int n; cin>>n;
-  Node root;
-  int prefixes = 0;
-  for(int i =0; i<n; i++)
-  {
-    string s; cin>>s;
-    root.addWord(s);
-  }
-  //iterateEdges(root)
-    //prefixes+=count(it->second);
-  return root.words;
+	int n; cin>>n; ct = 0;
+	Node root;
+	int prefixes = 0;
+	for(int i =0; i<n; i++)
+	{
+		string s; cin>>s;
+		root.addWord(s);
+	}
+	return ct;
 }
 int main()
 {
-  freopen("input","r",stdin);
-  int t; cin>>t;
-  for(int z = 1; z<=t; z++)
-    cout<<"Case #"<<z<<": "<<solve()<<"\n";
+	int t; cin>>t;
+	for(int z = 1; z<=t; z++)
+		cout<<"Case #"<<z<<": "<<solve()<<"\n";
 }
